7498c17803fb234750b19514ef16a1630865d0bc
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-09 19:21:28 +0530

    chore: setup pre-commit hooks with Husky (#4)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 
+import { includeIgnoreFile } from "@eslint/compat"
 import { FlatCompat } from "@eslint/eslintrc"
 import eslintPluginImport from "eslint-plugin-import"
 import storybook from "eslint-plugin-storybook"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url))
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
 const eslintConfig = [
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     plugins: {
