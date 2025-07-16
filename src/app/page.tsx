0dd80514d049da6efa59935a5cb3b6300611524b
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-16 23:19:35 +0530

    feat: add Navbar component (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 "use client"
 
-import Link from "@/components/ui/Link"
+import {
+  PiHouseFill,
+  PiArrowsDownUpFill,
+  PiChartDonutFill,
+  PiTipJarFill,
+  PiReceiptFill,
+} from "react-icons/pi"
+
+import { Navbar, NavbarItem } from "@/components/ui/Navbar"
 
 export default function Home() {
   return (
     <main className="grid gap-10">
-      <Link href="/test" className="font-bold">
-        This is a Link
-      </Link>
-      <Link href="/test" className="font-bold" withIcon={true}>
-        Sign Up
-      </Link>
+      <Navbar>
+        <NavbarItem
+          href="/"
+          label="Overview"
+          icon={PiHouseFill}
+          TEMP_CURRENT={true}
+        />
+        <NavbarItem href="/" label="Transactions" icon={PiArrowsDownUpFill} />
+        <NavbarItem href="/" label="Budgets" icon={PiChartDonutFill} />
+        <NavbarItem href="/" label="Pots" icon={PiTipJarFill} />
+        <NavbarItem href="/" label="Recurring Bills" icon={PiReceiptFill} />
+      </Navbar>
     </main>
   )
 }
