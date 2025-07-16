fd408357a393c1177d7afba56c9c7f49520cd536
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-16 10:51:13 +0530

    feat: add desktop layout for Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client"
-import Navbar from "@/components/ui/Navbar"
+import {
+  PiHouseFill,
+  PiArrowsDownUpFill,
+  PiChartDonutFill,
+  PiTipJarFill,
+  PiReceiptFill,
+} from "react-icons/pi"
+
+import { Navbar, NavbarItem } from "@/components/ui/Navbar"
 
 export default function Home() {
   return (
     <main className="grid gap-10">
-      <Navbar></Navbar>
+      <Navbar>
+        <NavbarItem
+          href="/"
+          label="Overview"
+          icon={PiHouseFill}
+          TEMP_CURRENT={true}
+        />
+        <NavbarItem href="/" label="Transactions" icon={PiArrowsDownUpFill} />
+        <NavbarItem href="/" label="Budgets" icon={PiChartDonutFill} />
+        <NavbarItem href="/" label="Pots" icon={PiTipJarFill} />
+        <NavbarItem href="/" label="Recurring Bills" icon={PiReceiptFill} />
+      </Navbar>
     </main>
   )
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,21 +1,28 @@
+import Image from "next/image"
 import NextLink from "next/link"
-import { ComponentProps } from "react"
-import { PiHouseFill } from "react-icons/pi"
+import { PiArrowFatLinesLeft } from "react-icons/pi"
 import { tv } from "tailwind-variants"
 
+// Most probably wll change how the logo SVG is imported and added
+import logoLarge from "../../../public/logo-large.svg"
+// import logoSmall from "../../../public/logo-small.svg"
+
+import type { ComponentProps, ReactNode } from "react"
 import type { IconType } from "react-icons"
 
-export default function Navbar() {
+export function Navbar({ children }: { children: ReactNode }) {
   return (
-    <nav className="bg-grey-900 flex items-center justify-between rounded-t-lg px-4 pt-2 lg:w-75 lg:flex-col lg:rounded-none lg:rounded-tr-lg lg:rounded-br-lg">
-      <NavbarItem
-        href="/"
-        label="Overview"
-        icon={PiHouseFill}
-        TEMP_CURRENT={true}
-      />
-      <NavbarItem href="/" label="Overview" icon={PiHouseFill} />
-      <NavbarItem href="/" label="Overview" icon={PiHouseFill} />
+    <nav className="bg-grey-900 rounded-t-lg px-4 pt-2 lg:grid lg:w-75 lg:gap-6 lg:rounded-none lg:rounded-r-lg lg:p-0 lg:pr-6">
+      <div className="hidden px-8 py-10 lg:block">
+        <Image src={logoLarge} alt="Finance" />
+      </div>
+      <div className="flex items-center justify-between lg:flex-col lg:items-start">
+        {children}
+      </div>
+      <button className="text-grey-300 hover:text-grey-100 flex cursor-pointer items-center justify-start gap-4 px-9 py-4">
+        <PiArrowFatLinesLeft className="size-6 shrink-0" />
+        <p className="text-base leading-normal font-bold">Minimize Menu</p>
+      </button>
     </nav>
   )
 }
@@ -23,14 +30,16 @@ export default function Navbar() {
 const navbarItemStyles = tv({
   slots: {
     navLink:
-      "text-grey-300 hover:text-grey-100 active:text-grey-100 border-grey-900 ring-grey-300 grid justify-items-center gap-1 rounded-t-lg border-b-4 px-4 pt-2 pb-2 transition-colors outline-none focus-visible:ring-3 md:px-5",
-    navLinkSpan: "hidden text-xs leading-normal font-bold md:block",
+      "text-grey-300 hover:text-grey-100 active:text-grey-100 border-grey-900 ring-grey-300 flex flex-col items-center gap-1 rounded-t-lg border-b-4 px-4 pt-2 pb-2 transition-colors outline-none focus-visible:ring-3 md:px-5 lg:w-full lg:flex-row lg:justify-start lg:gap-4 lg:rounded-none lg:rounded-r-lg lg:border-b-0 lg:border-l-4 lg:px-8 lg:py-4",
+    navLinkSpan:
+      "hidden text-xs leading-normal font-bold md:block lg:text-base",
   },
 
   variants: {
     isActive: {
       true: {
         navLink: "bg-beige-100 text-green border-green hover:text-green",
+        navLinkSpan: "text-grey-900",
       },
     },
   },
@@ -38,7 +47,7 @@ const navbarItemStyles = tv({
 
 const { navLink, navLinkSpan } = navbarItemStyles()
 
-function NavbarItem({
+export function NavbarItem({
   label,
   icon: Icon,
   TEMP_CURRENT = false,
@@ -51,7 +60,7 @@ function NavbarItem({
   return (
     <NextLink {...props} className={navLink({ isActive: TEMP_CURRENT })}>
       <Icon className="size-6 shrink-0" />
-      <span className={navLinkSpan()}>{label}</span>
+      <span className={navLinkSpan({ isActive: TEMP_CURRENT })}>{label}</span>
     </NextLink>
   )
 }
