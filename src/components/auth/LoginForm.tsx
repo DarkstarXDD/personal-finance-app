1f88de107ac2a0a5931e9ba4a16b20849068a826
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-07-28 20:38:18 +0530

    feat: add LoginForm validation

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,20 +1,60 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm, Controller } from "react-hook-form"
+
 import Button from "@/components/ui/Button"
 import Card from "@/components/ui/Card"
 import Heading from "@/components/ui/Heading"
 import Link from "@/components/ui/Link"
 import TextField from "@/components/ui/TextField"
+import { loginSchema } from "@/lib/schemas"
 
 export default function LoginForm() {
+  const { handleSubmit, control } = useForm({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(loginSchema),
+  })
+
   return (
     <Card className="mx-auto max-w-[35rem]">
       <div className="grid justify-items-center gap-8">
-        <form className="grid w-full gap-8">
+        <form
+          className="grid w-full gap-8"
+          onSubmit={handleSubmit(
+            (data) => console.log(data),
+            (errors) => console.log(errors)
+          )}
+        >
           <Heading as="h1">Login</Heading>
           <div className="grid gap-4">
-            <TextField label="Email" name="email" />
-            <TextField label="Password" type="password" name="password" />
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState: { invalid, error } }) => (
+                <TextField
+                  label="Email"
+                  {...field}
+                  autoComplete="email"
+                  isInvalid={invalid}
+                  errorMessage={error?.message}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({ field, fieldState: { invalid, error } }) => (
+                <TextField
+                  label="Password"
+                  type="password"
+                  {...field}
+                  autoComplete="email"
+                  isInvalid={invalid}
+                  errorMessage={error?.message}
+                />
+              )}
+            />
           </div>
           <Button type="submit" className="w-full">
             Login
