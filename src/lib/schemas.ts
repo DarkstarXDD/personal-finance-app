0b89f743a38ef0516a2d3fea3bc6b57496969e50
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-08-14 10:21:26 +0530

    chore: code formatting

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -5,14 +5,12 @@ export const signupSchema = z.object({
   email: z.email("Invalid email format").toLowerCase(),
   password: z.string().min(8, "Password must be at least 8 characters").trim(),
 })
-
 export type SignupSchema = z.infer<typeof signupSchema>
 
 export const loginSchema = z.object({
   email: z.email("Invalid email format").toLowerCase(),
   password: z.string().trim().min(1, "Password cannot be empty"),
 })
-
 export type LoginSchema = z.infer<typeof loginSchema>
 
 export const idSchema = z.object({ id: z.cuid() })
