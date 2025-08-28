383c40ac74920dcaf9174cc2b60825f18180041a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-08-28 16:17:50 +0530

    feat: desktop table for recurring bills

--- src/components/recurring-bills/TableDesktop.tsx
+++ src/components/recurring-bills/TableDesktop.tsx
@@ -1,5 +1,100 @@
-// import { useReactTable } from "@tanstack/react-table"
+"use client"
 
-export default function TableDesktop() {
-  return <div>TableDesktop</div>
+import {
+  useReactTable,
+  getCoreRowModel,
+  createColumnHelper,
+  flexRender,
+} from "@tanstack/react-table"
+
+import { RecurringBill } from "@/data-access/recurring-bills"
+
+const currencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
+
+const columnHelper = createColumnHelper<RecurringBill>()
+
+const columns = [
+  columnHelper.accessor("counterparty", {
+    header: "Bill Title",
+    cell: (data) => (
+      <span className="text-grey-900 text-sm leading-normal font-bold">
+        {data.getValue()}
+      </span>
+    ),
+  }),
+
+  columnHelper.accessor("dueDayOfMonth", {
+    header: "Due Date",
+    cell: (data) => (
+      <span className="text-green text-xs leading-normal font-normal">
+        {data.getValue()}
+      </span>
+    ),
+  }),
+
+  columnHelper.accessor("amount", {
+    header: () => <span className="block w-full text-end">Amount</span>,
+    cell: (data) => (
+      <span className="text-grey-900 block w-full text-end text-sm leading-normal font-bold">
+        {currencyFormat.format(Number(data.getValue()))}
+      </span>
+    ),
+  }),
+]
+
+export default function TableDesktop({
+  recurringBills,
+}: {
+  recurringBills: RecurringBill[]
+}) {
+  const table = useReactTable({
+    data: recurringBills,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+  return (
+    <div className="hidden w-full md:block">
+      <table className="w-full" style={{ minWidth: table.getTotalSize() }}>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="border-beige-100 border-b">
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="text-grey-500 px-4 pt-3 pb-5 text-start text-xs leading-normal font-normal"
+                  style={{ width: header.getSize() }}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr
+              key={row.id}
+              className="border-grey-100 hover:bg-beige-100 group border-b last:border-none"
+            >
+              {row.getAllCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  style={{ width: cell.column.getSize() }}
+                  className="px-4 py-4 group-last:pb-0"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
