cd2e714a98d31ab277fac7eebdea5e60716de6d6
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-08-29 20:53:26 +0530

    feat: add budget card and fetch budgets (#50)

--- src/app/(dashboard)/budgets/page.tsx
+++ src/app/(dashboard)/budgets/page.tsx
@@ -1,10 +1,13 @@
 import AddBudgetDialog from "@/components/budgets/AddBudgetDialog"
+import BudgetCard from "@/components/budgets/BudgetCard"
 import Heading from "@/components/ui/Heading"
+import { getBudgets } from "@/data-access/budgets"
 import { getCategories, getColors } from "@/data-access/lookups"
 
 export default async function RecurringBillsPage() {
   const categories = await getCategories()
   const colors = await getColors()
+  const budgets = await getBudgets()
 
   return (
     <main className="grid gap-8">
@@ -14,6 +17,10 @@ export default async function RecurringBillsPage() {
         </Heading>
         <AddBudgetDialog categories={categories} colors={colors} />
       </div>
+
+      {budgets.map((budget) => (
+        <BudgetCard key={budget.id} budget={budget} />
+      ))}
     </main>
   )
 }
---
+++ src/components/budgets/BudgetCard.tsx
@@ -0,0 +1,143 @@
+"use client"
+
+import { motion } from "motion/react"
+import { useState } from "react"
+import { ProgressBar } from "react-aria-components"
+
+import Card from "@/components/ui/Card"
+import Heading from "@/components/ui/Heading"
+import IconButton from "@/components/ui/IconButton"
+import Label from "@/components/ui/Label"
+import Link from "@/components/ui/Link"
+import { Menu, MenuTrigger, MenuItem } from "@/components/ui/Menu"
+import { currencyFormat } from "@/lib/utils"
+
+import type { Budget } from "@/data-access/budgets"
+
+export default function BudgetCard({ budget }: { budget: Budget }) {
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+
+  console.log(isEditDialogOpen)
+  console.log(isDeleteDialogOpen)
+
+  return (
+    <Card padding="lg" className="grid gap-5">
+      <div className="flex items-center justify-start gap-4">
+        <span
+          className="size-4 rounded-full"
+          style={{ backgroundColor: budget.color.value }}
+        />
+        <Heading as="h2" variant="secondary">
+          {budget.category.label}
+        </Heading>
+
+        <MenuTrigger>
+          <IconButton variant="options" className="ml-auto" />
+          <Menu>
+            <MenuItem onAction={() => setIsEditDialogOpen(true)}>
+              Edit Budget
+            </MenuItem>
+            <MenuItem
+              className="text-red"
+              onAction={() => setIsDeleteDialogOpen(true)}
+            >
+              Delete Budget
+            </MenuItem>
+          </Menu>
+        </MenuTrigger>
+      </div>
+
+      <div className="grid gap-4">
+        <ProgressBar value={15} minValue={0} maxValue={50}>
+          {({ percentage }) => (
+            <div className="grid gap-4">
+              <Label variant="secondary">
+                Maximum of {currencyFormat.format(Number(budget.maximumSpend))}
+              </Label>
+              <div className="bg-beige-100 flex h-8 w-full items-center rounded-sm p-1">
+                <motion.div
+                  className="h-full rounded-sm"
+                  style={{ backgroundColor: budget.color.value }}
+                  initial={{ width: 0 }}
+                  animate={{ width: percentage + "%" }}
+                  transition={{ delay: 0.18 }}
+                />
+              </div>
+            </div>
+          )}
+        </ProgressBar>
+
+        <dl className="grid grid-cols-2">
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1">
+            <span
+              className="row-span-2 h-full w-1 rounded-lg"
+              style={{ backgroundColor: budget.color.value }}
+            />
+            <dt className="text-grey-500 text-xs leading-normal font-normal">
+              Spent
+            </dt>
+            <dd className="text-grey-900 text-sm leading-normal font-bold">
+              $25.00
+            </dd>
+          </div>
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1">
+            <span className="bg-beige-100 row-span-2 h-full w-1 rounded-lg" />
+            <dt className="text-grey-500 text-xs leading-normal font-normal">
+              Free
+            </dt>
+            <dd className="text-grey-900 text-sm leading-normal font-bold">
+              $50.00
+            </dd>
+          </div>
+        </dl>
+
+        <div className="bg-beige-100 grid gap-5 rounded-xl p-4 md:p-5">
+          <div className="flex justify-between">
+            <h3 className="text-grey-900 text-base leading-normal font-bold">
+              Latest Spending
+            </h3>
+            <Link href="/" withIcon>
+              See All
+            </Link>
+          </div>
+          <ul>
+            <li className="border-grey-200 grid grid-cols-2 gap-1 border-b py-3 first:pt-0 last:border-none last:pb-0">
+              <h4 className="text-grey-900 row-span-2 text-xs leading-normal font-bold">
+                Papa Software
+              </h4>
+              <p className="text-grey-900 justify-self-end text-xs leading-normal font-bold">
+                -$10.00
+              </p>
+              <p className="text-grey-500 justify-self-end text-xs leading-normal font-normal">
+                16 Aug 2024
+              </p>
+            </li>
+            <li className="border-grey-200 grid grid-cols-2 border-b py-3 first:pt-0 last:border-none last:pb-0">
+              <h4 className="text-grey-900 row-span-2 text-xs leading-normal font-bold">
+                Papa Software
+              </h4>
+              <p className="text-grey-900 justify-self-end text-xs leading-normal font-bold">
+                -$10.00
+              </p>
+              <p className="text-grey-500 justify-self-end text-xs leading-normal font-normal">
+                16 Aug 2024
+              </p>
+            </li>
+            <li className="border-grey-200 grid grid-cols-2 border-b py-3 first:pt-0 last:border-none last:pb-0">
+              <h4 className="text-grey-900 row-span-2 text-xs leading-normal font-bold">
+                Papa Software
+              </h4>
+              <p className="text-grey-900 justify-self-end text-xs leading-normal font-bold">
+                -$10.00
+              </p>
+              <p className="text-grey-500 justify-self-end text-xs leading-normal font-normal">
+                16 Aug 2024
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Card>
+  )
+}
