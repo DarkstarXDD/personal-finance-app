80e8dd284916a6e323686dca72b00b855d8e23b2
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-04 23:13:42 +0530

    feat: budget summary (#61)

--- src/app/(dashboard)/budgets/page.tsx
+++ src/app/(dashboard)/budgets/page.tsx
@@ -1,5 +1,6 @@
 import AddBudgetDialog from "@/components/budgets/AddBudgetDialog"
 import BudgetCard from "@/components/budgets/BudgetCard"
+import BudgetsSummary from "@/components/budgets/BudgetsSummary"
 import Heading from "@/components/ui/Heading"
 import { getBudgets } from "@/data-access/budgets"
 import { getCategories, getColors } from "@/data-access/lookups"
@@ -27,14 +28,19 @@ export default async function BudgetsPage() {
         <AddBudgetDialog categories={categories} colors={colors} />
       </div>
 
-      {budgetsWithTransactions.map(async (budget) => (
-        <BudgetCard
-          key={budget.id}
-          budget={budget}
-          categories={categories}
-          colors={colors}
-        />
-      ))}
+      <div className="grid gap-6 2xl:grid-cols-[2fr_3fr] 2xl:items-start">
+        <BudgetsSummary budgets={budgetsWithTransactions} />
+        <div className="grid gap-6">
+          {budgetsWithTransactions.map(async (budget) => (
+            <BudgetCard
+              key={budget.id}
+              budget={budget}
+              categories={categories}
+              colors={colors}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
