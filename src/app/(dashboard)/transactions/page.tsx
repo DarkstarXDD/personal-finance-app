dab9fc0769db620efa92b5696ffb3eeb5101219e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-09 10:52:38 +0530

    feat: empty state for transactions page

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,7 +1,10 @@
+import { PiWarningCircleFill } from "react-icons/pi"
+
 import AddTransactionDialog from "@/components/transactions/AddTransactionDialog"
 import TableDesktop from "@/components/transactions/TableDesktop"
 import TableFilters from "@/components/transactions/TableFilters"
 import TableMobile from "@/components/transactions/TableMobile"
+import TransactionsEmptyState from "@/components/transactions/TransactionsEmptyState"
 import Card from "@/components/ui/Card"
 import Heading from "@/components/ui/Heading"
 import Pagination from "@/components/ui/Pagination"
@@ -25,12 +28,29 @@ export default async function TransactionsPage({
   const {
     transactions,
     pagination: { totalPages },
+    totalItemsWithoutFiltering,
   } = await getTransactions({
     query,
     sortby,
     category,
     currentPage,
   })
+
+  if (totalItemsWithoutFiltering === 0) {
+    return (
+      <main className="grid gap-8">
+        <div className="flex items-center justify-between gap-2">
+          <Heading as="h1" variant="primary">
+            Transactions
+          </Heading>
+          <AddTransactionDialog categories={categories} />
+        </div>
+        <Card>
+          <TransactionsEmptyState />
+        </Card>
+      </main>
+    )
+  }
 
   return (
     <main className="grid gap-8">
@@ -40,10 +60,23 @@ export default async function TransactionsPage({
         </Heading>
         <AddTransactionDialog categories={categories} />
       </div>
+
       <Card className="grid gap-6">
         <TableFilters categories={categories} />
-        <TableMobile transactions={transactions} />
-        <TableDesktop transactions={transactions} />
+        {transactions.length > 0 ? (
+          <>
+            <TableMobile transactions={transactions} />
+            <TableDesktop transactions={transactions} />
+          </>
+        ) : (
+          <div className="flex items-center justify-center gap-2">
+            <PiWarningCircleFill className="text-grey-500 size-5" />
+            <p className="text-grey-500 text-center text-sm leading-normal font-normal">
+              No results match your filters.
+            </p>
+          </div>
+        )}
+
         <Pagination totalPages={totalPages} />
       </Card>
     </main>
--- src/components/overview/TransactionsOverview.tsx
+++ src/components/overview/TransactionsOverview.tsx
@@ -1,6 +1,6 @@
 import { format } from "date-fns"
-import { PiArrowsDownUpFill } from "react-icons/pi"
 
+import TransactionsEmptyState from "@/components/transactions/TransactionsEmptyState"
 import Card from "@/components/ui/Card"
 import Heading from "@/components/ui/Heading"
 import Link from "@/components/ui/Link"
@@ -45,22 +45,5 @@ export default async function TransactionsOverview() {
         )}
       </div>
     </Card>
-  )
-}
-
-function TransactionsEmptyState() {
-  return (
-    <div className="grid justify-items-center gap-3 py-12 text-center">
-      <div className="bg-beige-100 flex h-16 w-16 items-center justify-center rounded-full">
-        <PiArrowsDownUpFill className="text-grey-500 size-8" />
-      </div>
-      <Heading as="h3" variant="secondary">
-        No transactions yet
-      </Heading>
-      <p className="text-grey-500 text-sm leading-normal font-normal">
-        Your transaction history will appear here once you start making
-        purchases.
-      </p>
-    </div>
   )
 }
