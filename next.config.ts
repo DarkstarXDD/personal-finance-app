9e35d712028ae9d25ba2f64dfd64d2db1303f623
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-12 12:31:34 +0530

    chore: try removing the config part

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
-// @ts-expect-error no types
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
 
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.plugins = [...config.plugins, new PrismaPlugin()]
+  //   }
+  //   return config
+  // },
 }
 
 export default nextConfig
