b667284d07053e8d0e3351f0f5a21584a91ae54c
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-12 12:44:25 +0530

    chore: add the config code back

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
+// @ts-expect-error no types
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
 
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.plugins = [...config.plugins, new PrismaPlugin()]
-  //   }
-  //   return config
-  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 }
 
 export default nextConfig
