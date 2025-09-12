8f9795b57b92a0f9f65bdc26c3e784a212f2dccb
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-12 13:06:35 +0530

    fix: prisma query engine not found bug (#72)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+// @ts-expect-error no types
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
+
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 }
 
 export default nextConfig
