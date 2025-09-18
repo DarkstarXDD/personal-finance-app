3c35702e64ac885ac1611b265919e25d6a92910e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-18 16:53:53 +0530

    test: login

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,19 @@
 import { test, expect } from "@playwright/test"
 
-test("grab element", async ({ page }) => {
+test("login", async ({ page }) => {
   await page.goto("http://localhost:3000/")
 
   await expect(
-    page.getByRole("heading", { name: "Loogin", level: 1 })
+    page.getByRole("heading", { name: "Login", level: 1 })
+  ).toBeVisible()
+
+  await page.getByRole("textbox", { name: "Email" }).fill("peter@email.com")
+
+  await page.getByRole("textbox", { name: "Password" }).fill("Peter1234")
+
+  await page.getByRole("button", { name: "Login" }).click()
+
+  await expect(
+    page.getByRole("heading", { name: "Overview", level: 1 })
   ).toBeVisible()
 })
