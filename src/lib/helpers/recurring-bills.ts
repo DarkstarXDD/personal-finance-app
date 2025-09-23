5b1c8485558df11c84812ebd37c8b8a8845030a1
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-23 20:03:49 +0530

    refactor: move due dates to next month if already passed

--- src/lib/helpers/recurring-bills.ts
+++ src/lib/helpers/recurring-bills.ts
@@ -1,16 +1,29 @@
-import { lastDayOfMonth, differenceInCalendarDays } from "date-fns"
+import { lastDayOfMonth, differenceInCalendarDays, addMonths } from "date-fns"
 
 export function getDueDate(subscriptionStartDate: Date) {
+  // If the bill was first created on a 31st, in months 31st
+  // does not exist, the bill will happen on 30th.
   const today = new Date()
   const year = today.getFullYear()
   const month = today.getMonth()
 
+  // 1. Find the created day (the exact day the bill was first created)
   const createdDay = subscriptionStartDate.getDate()
+
+  // 2. Find the last day of the current month
   const lastDayOfCurrentMonth = lastDayOfMonth(new Date(year, month))
 
+  // 3. Use whichever is the minimum from the above two.
   const day = Math.min(lastDayOfCurrentMonth.getDate(), createdDay)
 
-  const dueDate = new Date(year, month, day)
+  let dueDate = new Date(year, month, day)
+
+  // If the due date is already passed for the current month
+  // add 1 month to it and return the due date of the next month
+  const daysUntilDue = getDaysUntilDue(dueDate)
+  if (daysUntilDue < 0) {
+    dueDate = addMonths(dueDate, 1)
+  }
 
   return dueDate
 }
