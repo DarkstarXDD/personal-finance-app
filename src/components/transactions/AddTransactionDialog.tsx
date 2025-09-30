88a5ebe3ee0de4d518637e6925bd0dfb16e02dac
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-09-30 16:00:07 +0530

    refactor: revert layout change done in a previous commit

--- src/components/transactions/AddTransactionDialog.tsx
+++ src/components/transactions/AddTransactionDialog.tsx
@@ -109,6 +109,21 @@ export default function AddTransactionDialog({
               />
 
               <Controller
+                name="amount"
+                control={control}
+                render={({ field, fieldState: { invalid, error } }) => (
+                  <NumberField
+                    label="Transaction Amount"
+                    {...field}
+                    isInvalid={invalid}
+                    errorMessage={error?.message}
+                    isDisabled={isSubmitting}
+                    formatOptions={{ style: "currency", currency: "USD" }}
+                  />
+                )}
+              />
+
+              <Controller
                 name="categoryId"
                 control={control}
                 render={({
@@ -133,21 +148,6 @@ export default function AddTransactionDialog({
                       </SelectItem>
                     )}
                   </Select>
-                )}
-              />
-
-              <Controller
-                name="amount"
-                control={control}
-                render={({ field, fieldState: { invalid, error } }) => (
-                  <NumberField
-                    label="Transaction Amount"
-                    {...field}
-                    isInvalid={invalid}
-                    errorMessage={error?.message}
-                    isDisabled={isSubmitting}
-                    formatOptions={{ style: "currency", currency: "USD" }}
-                  />
                 )}
               />
 
