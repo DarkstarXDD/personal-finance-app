1720b58f009b37c37fee968f1ec5519d0f064a97
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-10-29 14:05:36 +0530

    style: update button disabled styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,15 +10,15 @@ import { tv, type VariantProps } from "tailwind-variants"
 import Spinner from "@/components/ui/Spinner"
 
 const buttonStyles = tv({
-  base: "rac-focus-visible:ring-2 rac-focus-visible:ring-offset-2 rac-disabled:cursor-not-allowed rac-pending:cursor-not-allowed flex cursor-pointer items-center justify-center gap-2 rounded-lg leading-normal font-semibold transition-colors outline-none",
+  base: "rac-focus-visible:ring-2 rac-focus-visible:ring-offset-2 rac-disabled:cursor-not-allowed rac-pending:cursor-not-allowed rac-disabled:text-disabled_subtle rac-disabled:bg-disabled_subtle rac-disabled:border-disabled flex cursor-pointer items-center justify-center gap-2 rounded-lg border leading-normal font-semibold transition-colors outline-none",
   variants: {
     variant: {
       primary:
-        "bg-brand-solid border-brand-solid rac-hover:border-brand-solid_hover ring-brand rac-hover:bg-brand-solid_hover rac-pressed:bg-brand-solid_hover rac-pressed:border-brand-solid_hover rac-disabled:bg-primary rac-disabled:text-disabled_subtle rac-disabled:border-disabled border text-white",
+        "bg-brand-solid border-brand-solid rac-hover:border-brand-solid_hover ring-brand rac-hover:bg-brand-solid_hover rac-pressed:bg-brand-solid_hover rac-pressed:border-brand-solid_hover text-white",
       secondary:
-        "text-secondary ring-brand bg-primary border-primary rac-hover:bg-primary_hover rac-pressed:bg-primary_hover rac-disabled:text-disabled_subtle rac-disabled:border-disabled border",
+        "text-secondary ring-brand bg-primary border-primary rac-hover:bg-primary_hover rac-pressed:bg-primary_hover",
       destructive:
-        "bg-error-solid rac-disabled:bg-disabled rac-disabled:border-disabled border-error-solid rac-hover:border-error-solid_hover rac-hover:bg-error-solid_hover rac-pressed:bg-error-solid_hover ring-error rac-disabled:text-disabled_subtle border text-white",
+        "bg-error-solid border-error-solid rac-hover:border-error-solid_hover rac-hover:bg-error-solid_hover rac-pressed:bg-error-solid_hover ring-error text-white",
     },
     size: {
       md: "min-h-10 px-3.5 py-2 text-sm",
