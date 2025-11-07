1e5dd2c2ba254a0c2f0e9bf990ca35b4bd733e1c
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-11-07 18:17:09 +0530

    feat: add PotsSummary component

--- src/app/(dashboard)/pots/page.tsx
+++ src/app/(dashboard)/pots/page.tsx
@@ -6,6 +6,7 @@ import Card from "@/components/ui/Card"
 import { getColors } from "@/data-access/lookups"
 import AddPotDialog from "@/features/pots/components/AddPotDialog"
 import PotCard from "@/features/pots/components/PotCard"
+import PotsSummary from "@/features/pots/components/PotsSummary"
 import { getPots } from "@/features/pots/data-access"
 
 import type { Metadata } from "next"
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
 
 export default async function PotsPage() {
   const colors = await getColors()
-  const pots = await getPots()
+  const { pots, potsSummary } = await getPots()
 
   return (
     <main className="@container grid gap-8">
@@ -27,10 +28,19 @@ export default async function PotsPage() {
       />
 
       {pots.length > 0 ? (
-        <div className="grid gap-6 @3xl:grid-cols-2">
-          {pots.map((pot) => (
-            <PotCard key={pot.id} pot={pot} colors={colors} />
-          ))}
+        <div className="grid items-start gap-6 @6xl:grid-cols-[2fr_5fr]">
+          <Card size="lg" className="@container grid content-start gap-8">
+            <h2 className="text-primary text-lg leading-tight font-semibold">
+              Pots Summary
+            </h2>
+            <PotsSummary {...{ pots, potsSummary }} />
+          </Card>
+
+          <div className="grid gap-6 @3xl:grid-cols-2">
+            {pots.map((pot) => (
+              <PotCard key={pot.id} pot={pot} colors={colors} />
+            ))}
+          </div>
         </div>
       ) : (
         <Card>
