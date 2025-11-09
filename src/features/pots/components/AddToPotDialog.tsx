bdb38aa7d35c2dc3ce5ca529f666d3c221a27910
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-11-09 17:19:04 +0530

    style: update AddToPotDialog

--- src/features/pots/components/AddToPotDialog.tsx
+++ src/features/pots/components/AddToPotDialog.tsx
@@ -8,15 +8,15 @@ import { useForm, Controller } from "react-hook-form"
 
 import Button from "@/components/ui/Button"
 import { DialogTrigger, Dialog } from "@/components/ui/Dialog"
-import Label from "@/components/ui/FieldLabel"
 import NumberField from "@/components/ui/NumberField"
 import { addToPot } from "@/features/pots/actions"
+import { type Pot } from "@/features/pots/data-access"
 import { potAmountUpdateSchema } from "@/lib/schemas"
 import { setErrorsFromServer } from "@/lib/utils"
 
-import type { Pot } from "@/features/pots/data-access"
+type AddToPotDialogProps = { pot: Pot }
 
-export default function AddToPotDialog({ pot }: { pot: Pot }) {
+export default function AddToPotDialog({ pot }: AddToPotDialogProps) {
   const form = useForm({
     resolver: zodResolver(potAmountUpdateSchema),
     defaultValues: { id: pot.id, amountToUpdate: 0 },
@@ -34,14 +34,14 @@ export default function AddToPotDialog({ pot }: { pot: Pot }) {
       <Button variant="secondary" size="lg" className="w-full">
         + Add
       </Button>
-      <Dialog title={`Add to ‘${pot.name}’`}>
+      <Dialog
+        title={`Add to ‘${pot.name}’`}
+        description="Add money to your pot to grow your savings and get closer to your goal"
+      >
         {({ close }) => (
           <div className="grid gap-5">
-            <p className="text-grey-500 text-sm leading-normal font-normal">
-              Add money to your pot to grow your savings and get closer to your
-              goal.
-            </p>
             <ProgressBar
+              aria-label="New Amount"
               value={draftAmountInPot}
               minValue={0}
               maxValue={pot.target}
@@ -57,16 +57,17 @@ export default function AddToPotDialog({ pot }: { pot: Pot }) {
                 )
 
                 return (
-                  <div className="grid gap-4">
+                  <div className="grid gap-1.5">
                     <div className="flex items-center justify-between">
-                      <Label>New Amount</Label>
-                      <span className="text-grey-900 text-3xl leading-tight font-bold">
+                      <p className="text-sm font-semibold">New Amount</p>
+                      <span className="text-primary text-2xl leading-tight font-semibold">
                         {valueText}
                       </span>
                     </div>
-                    <div className="bg-beige-100 flex h-2 rounded">
+
+                    <div className="bg-quaternary flex h-2 rounded">
                       <motion.div
-                        className="bg-grey-900 h-full shrink-0 rounded-l"
+                        className="bg-brand-solid h-full shrink-0 rounded-l"
                         animate={{
                           width: currentAmountAsPercentage + "%",
                         }}
@@ -79,20 +80,18 @@ export default function AddToPotDialog({ pot }: { pot: Pot }) {
                         }}
                       />
                     </div>
-                    <div className="text-grey-500 flex items-center justify-between text-xs leading-normal">
-                      <span className="font-bold">
-                        {Math.round(percentage ?? 0)}%
-                      </span>
-                      <span className="font-normal">
-                        Target of ${pot.target}
-                      </span>
+
+                    <div className="flex items-center justify-between text-xs font-medium">
+                      <span>{Math.round(percentage ?? 0)}%</span>
+                      <span>Target of ${pot.target}</span>
                     </div>
                   </div>
                 )
               }}
             </ProgressBar>
+
             <form
-              className="grid gap-5"
+              className="grid gap-6"
               onSubmit={form.handleSubmit(async (data) => {
                 const response = await addToPot(data)
                 if (response) {
@@ -117,9 +116,11 @@ export default function AddToPotDialog({ pot }: { pot: Pot }) {
                   />
                 )}
               />
+
               <Button
-                type="submit"
                 variant="primary"
+                type="submit"
+                size="lg"
                 isPending={form.formState.isSubmitting}
               >
                 Confirm Addition
