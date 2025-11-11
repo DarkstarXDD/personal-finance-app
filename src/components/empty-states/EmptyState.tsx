78f4af49fe7ddefd7bc03e5c223bd9a94358ff92
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-11-11 15:08:57 +0530

    style: update Empty state styles

--- src/components/empty-states/EmptyState.tsx
+++ src/components/empty-states/EmptyState.tsx
@@ -1,27 +1,26 @@
 import { IconType } from "react-icons"
 
-import Heading from "@/components/ui/Heading"
+type EmptyStateProps = {
+  icon: IconType
+  title: string
+  description: string
+}
 
 export default function EmptyState({
   icon: Icon,
   title,
   description,
-}: {
-  icon: IconType
-  title: string
-  description: string
-}) {
+}: EmptyStateProps) {
   return (
-    <div className="grid justify-items-center gap-3 py-12 text-center">
-      <div className="bg-beige-100 flex h-16 w-16 items-center justify-center rounded-full">
-        <Icon className="text-grey-500 size-8" />
+    <div className="grid justify-items-center gap-4 py-12 text-center">
+      <div className="bg-disabled flex h-16 w-16 items-center justify-center rounded-full">
+        <Icon className="text-fg-quaternary size-8" />
       </div>
-      <Heading as="h3" variant="secondary">
-        {title}
-      </Heading>
-      <p className="text-grey-500 text-sm leading-normal font-normal">
-        {description}
-      </p>
+
+      <div className="grid gap-1">
+        <h2 className="text-primary text-center font-semibold">{title}</h2>
+        <p className="text-tertiary text-center text-sm">{description}</p>
+      </div>
     </div>
   )
 }
