fcaaef957c58af5607c6519c8738e53f9921d014
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-11-11 15:22:31 +0530

    deps: remove nextjs prisma workaround plugin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin"
-
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
@@ -9,13 +7,6 @@ const nextConfig: NextConfig = {
   },
 
   // cacheComponents: true,
-
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.plugins = [...config.plugins, new PrismaPlugin()]
-  //   }
-  //   return config
-  // },
 }
 
 export default nextConfig
