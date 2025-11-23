7e2337cf1e8e33715ba4b94de766ab22e2f71929
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-11-23 10:42:59 +0530

    db: add user role filed

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -27,7 +27,7 @@ export async function verifyToken(token: string) {
 }
 
 export async function createSession(payload: { userId: string }) {
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
   const sessionToken = await createToken(payload)
   const cookieStore = await cookies()
   cookieStore.set({
